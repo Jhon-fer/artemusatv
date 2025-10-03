3b7aeef5d0b6d0996e55f2b337f7389ebaa829e8
jhon fernando gomez quispe <1613201@senati.pe>
2025-10-03 10:29:57 -0500

    Delete prueba directory

--- prueba/principal/pogramasN/js/java.js
+++
@@ -1,8 +0,0 @@
-// Selecciona el toggle y el menú
-const toggle = document.querySelector(".menu-toggle");
-const menu = document.querySelector("nav ul");
-
-// Evento click para mostrar/ocultar el menú
-toggle.addEventListener("click", () => {
-    menu.classList.toggle("active");
-});
