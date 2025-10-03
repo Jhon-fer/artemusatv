bc369968f88bddf79446964a418a4d01fa02eb89
jhon fernando gomez quispe <jhonfernandogomezquispe@gmail.com>
2025-10-03 10:30:33 -0500

    Delete Modern-Login-master directory

--- Modern-Login-master/verificar.php
+++
@@ -1,6 +0,0 @@
-<form action="procesar_verificacion.php" method="POST">
-    <input type="hidden" name="correo" value="<?php echo $_GET['correo']; ?>">
-    <label>Código de verificación:</label>
-    <input type="text" name="codigo" required>
-    <button type="submit">Verificar</button>
-</form>
